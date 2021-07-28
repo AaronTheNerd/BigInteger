f4fbc07d6f84e5a630a178fac2cf8e1507ab4b7f
AaronTheNerd <bargeaaron@gmail.com>
2021-07-27 20:36:53 -0400

    Removed unfinished projects

--- main.cpp
+++ main.cpp
@@ -1,129 +1,32 @@
 // Written by Aaron Barge
-// Copyright 2019
+// Copyright 2020
+
+// This is a simple test program that can help show the precision of BigInt
+// This program gives the smallest positive number that is divisible by all
+// numbers between 1 and num
 
 
-#include <assert.h>
 #include <iostream>
+#include <assert.h>
 #include "BigInt.hpp"
+ 
+void min_divis(unsigned long long int num) {
+  BigInt result(1);
+  unsigned long long int nums[num + 1];
+  for (unsigned long long int i = 0; i < num + 1; ++i) {
+    nums[i] = i;
+  }
+  for (unsigned long long int i = 2; i < num + 1; ++i) {
+    result *= nums[i];
+    for (unsigned long long int j = 2 * i; j < num + 1; j += i) {
+      if (nums[j] % nums[i] == 0)
+        nums[j] /= nums[i];
+    }
+  }
+  std::cout << "The minimum number divisible by every number between 1 and " << num << ": " << result << std::endl;
+}
 
 
-struct Testing {
-  bool plus_eq() {
-    std::cout << "Testing Plus Equals: ";
-    BigInt i(100), j(200);
-    bool result = (i += j) == (100 + 200);
-    if (result) {
-      std::cout << "SUCCESS" << std::endl;
-    } else {
-      std::cout << "FAILED" << std::endl;
-    }
-    return result;
-  }
-  bool minus_eq() {
-    BigInt i(200), j(100);
-    std::cout << "Testing Minus Equals: ";
-    bool result = (i -= j) == (200 - 100);
-    if (result) {
-      std::cout << "SUCCESS" << std::endl;
-    } else {
-      std::cout << "FAILED" << std::endl;
-    }
-    return result;
-  }
-  bool times_eq() {
-    BigInt i(100), j(200);
-    std::cout << "Testing Times Equals: ";
-    bool result = (i *= j) == (100 * 200);
-    if (result) {
-      std::cout << "SUCCESS" << std::endl;
-    } else {
-      std::cout << "FAILED" << std::endl;
-    }
-    return result;
-  }
-  bool divide_eq() {
-    BigInt i(200), j(100);
-    std::cout << "Testing Divide Equals: ";
-    bool result = (i /= j) == (200 / 100);
-    if (result) {
-      std::cout << "SUCCESS" << std::endl;
-    } else {
-      std::cout << "FAILED" << std::endl;
-    }
-    return result;
-  }
-  bool mod_eq() {
-    BigInt i(200), j(100);
-    std::cout << "Testing Mod Equals: ";
-    bool result = (i %= j) == (200 % 100);
-    if (result) {
-      std::cout << "SUCCESS" << std::endl;
-    } else {
-      std::cout << "FAILED" << std::endl;
-    }
-    return result;
-  }
-  bool less_than() {
-    BigInt i(100), j(200), k(100);
-    std::cout << "Testing Less Than: ";
-    bool result =  ((i < k) == (100 < 100)) && ((i < j) == (100 < 200));
-    if (result) {
-      std::cout << "SUCCESS" << std::endl;
-    } else {
-      std::cout << "FAILED" << std::endl;
-    }
-    return result;
-  }
-  bool greater_than() {
-    BigInt i(100), j(200), k(100);
-    std::cout << "Testing Greater Than: ";
-    bool result =  ((i > k) == (100 > 100)) && ((j > i) == (200 > 100));
-    if (result) {
-      std::cout << "SUCCESS" << std::endl;
-    } else {
-      std::cout << "FAILED" << std::endl;
-    }
-    return result;
-  }
-  bool less_than_eqs() {
-    BigInt i(100), j(200), k(100);
-    std::cout << "Testing Less Than or Equal To: ";
-    bool result =  ((i <= k) == (100 <= 100)) && ((i <= j) == (100 <= 200)) && ((j <= i) == (200 <= 100));
-    if (result) {
-      std::cout << "SUCCESS" << std::endl;
-    } else {
-      std::cout << "FAILED" << std::endl;
-    }
-    return result;
-  }
-  bool greater_than_eqs() {
-    BigInt i(100), j(200), k(100);
-    std::cout << "Testing Greater Than or Equal To: ";
-    bool result =  ((i >= k) == (100 >= 100)) && ((i >= j) == (100 >= 200)) && ((j >= i) == (200 >= 100));
-    if (result) {
-      std::cout << "SUCCESS" << std::endl;
-    } else {
-      std::cout << "FAILED" << std::endl;
-    }
-    return result;
-  }
-  Testing() {
-    bool result = true;
-    result &= plus_eq();
-    result &= minus_eq();
-    result &= times_eq();
-    result &= divide_eq();
-    result &= mod_eq();
-    result &= less_than();
-    result &= greater_than();
-    result &= less_than_eqs();
-    result &= greater_than_eqs();
-    assert(result);
-  }
-};
-
-
-int main(int argc, char** argv) {
-  Testing t;
-  return 0;
+int main() {
+  min_divis(5000);
 }
